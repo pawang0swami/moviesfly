38e3a343ebfee990b799f49e5a8836cfe75a09bf
PAWAN GOSWAMI <pawangoswami6823@gmail.com>
2025-06-28 15:18:57 +0530

    completed

--- back/api/index.js
+++ back/api/index.js
@@ -1,46 +1,80 @@
-const Data =require ("../db/data")
+const Data = require("../db/data");
 const Users1 = require("../db/users");
 require("../db/connect");
 const cors = require("cors");
 
-const express= require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 
-app.post ("/singup",async(req, resp)=>{
-let user2 = new Users1(req.body);
-  let result = await user2.save();
-  // result = result.toObject();
- console.log(result);
+app.post("/singup", async (req, resp) => {
+  let user2 = new Users1(req.body);
+  const result = await user2.save();
   resp.send(result);
   // delete result.password;
+});
+app.get("/data", async (req, resp) => {
+  let r = await Data.find();
+  resp.send(r);
+});
+
+app.post("/data", async (req, resp) => {
+  let d = new Data(req.body);
+  let d1 = await d.save();
+  resp.send(d1);
+  //  console.log(d1);
+});
+
+// app.post("/login",async(req,resp)=>{
+//  const {email,password}=req.body
+//   let d1 = await Users1.findOne(email,password)
+
+//   //   //  resp.send(user)
+// //    let result = await user.save();
+// //   result = result.toObject();
+// //  console.log(result);
+// //   resp.send(result);
+
+//  if(d1){
+// resp.send(d1)
+//  }
+//  else{
+//   resp.status(401).send({ error: "Invalid email or password" });
+//  }
+// })
+
+app.post("/login", async (req, resp) => {
+  const { email, password } = req.body;
+  const user = await Users1.findOne({ email, password }).select("-password");
+  if (user) {
+    resp.send(user);
+  }
+});
+
+app.get("/movie/:id", async (r, R) => {
+  let a = await Data.findOne({ _id: r.params.id });
+  R.send(a);
+});
+
+app.put("/edit/:id",async(r,R)=>{
+  let a =await Data.updateOne({_id:r.params.id},{$set:r.body})
+     R.send(a)
 })
-app.get("/data",async(req,resp)=>{
-   let r= await Data.find()
-    resp.send(r)
+
+
+app.get("/edit/:id",async(r,R)=>{
+  let a= await Data.findOne({_id:r.params.id});
+  R.send(a)
 })
 
-app.post("/data",async(req,resp)=>{
-  let d=new Data(req.body)
-  let d1=await d.save()
-  resp.send(d1)
- console.log(d1);
-
+app.delete("/delete/:id", async(r,R)=>{
+let a= await Data.deleteOne({_id:r.params.id})
+R.send(a)
 })
 
-app.post("/login",async(req,resp)=>{
-  // let d= await Users1.find()
- 
-  let user = await Users1.findOne(req.body).select("password");
-   resp.send(user)
-})
-    
-
-
-
-app. listen (4010,()=>{
-    console.log("ok")
-})
+app.listen(4010, () => {
+  console.log("ok");
+});
